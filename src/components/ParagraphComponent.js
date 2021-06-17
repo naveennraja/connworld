49c8fa010d43ccd1ead5a988ac81627aa7030379
Naveen Nata Raja <naveennraja@gmail.com>
2021-06-17 11:46:16 +0100

    Changes on jdom

--- src/components/ParagraphComponent.js
+++ src/components/ParagraphComponent.js
@@ -4,13 +4,16 @@ import { JSDOM } from 'jsdom';
 const ParagraphComponent = (props) => {
   const window = new JSDOM('').window;
   const DOMPurify = createDOMPurify(window);
+  const clean = DOMPurify.sanitize(props.text);
+
+  const dom = new JSDOM(props.text);
+  //const window = new JSDOM('').window;
+  //const DOMPurify = createDOMPurify(dom.window);
+  console.log('Dom', dom);
+  DOMPurify.sanitize(dom.window.document.body, { RETURN_DOM: true });
   return (
     <div className='text-justify text-black  py-4'>
-      {
-        <div
-          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(props.text) }}
-        />
-      }
+      {<div dangerouslySetInnerHTML={{ __html: clean }} />}
     </div>
   );
 };
